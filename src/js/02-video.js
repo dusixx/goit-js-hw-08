7176a64b8006508f9db159cda2b6783308fb8f13
dusixx <113414935+dusixx@users.noreply.github.com>
2023-02-07 15:33:25 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,17 +2,20 @@ import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
 const THROTTLE_PERIOD = 1000;
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
 const playerRef = document.querySelector('#vimeo-player');
 const player = new Player(playerRef);
 
 // ставим текущую позицию воспроизведения
-const playerCurrentTime = localStorage.getItem('videoplayer-current-time');
+const playerCurrentTime = localStorage.getItem(CURRENT_TIME_KEY);
 player.setCurrentTime(playerCurrentTime || 0);
 
 player.on(
   'timeupdate',
-  throttle(data => {
-    // раз в секунду сохраняем текущую позицию воспроизведения
-    localStorage.setItem('videoplayer-current-time', data.seconds);
-  }, THROTTLE_PERIOD)
+  throttle(
+    // сохраняем текущую позицию воспроизведения
+    data => localStorage.setItem(CURRENT_TIME_KEY, data.seconds),
+    THROTTLE_PERIOD
+  )
 );
