c519b7d71aab2a4d5319e6cb316f1378f03b92d5
dusixx <113414935+dusixx@users.noreply.github.com>
2023-02-07 14:21:52 +0200

    adds task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+createGallery(galleryItems, 'gallery');
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+/**
+ * Создает разметку галлереи на основе массива items
+ * и вставляет ее в элемент с классом className
+ */
+function createGallery(items, className) {
+  const galleryRef = document.querySelector(`.${className}`);
+
+  if (!galleryRef) return null;
+
+  // создаем разметку
+  const markup = items
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="${className}__item" href="${original}">
+            <img class="${className}__image" src="${preview}" alt="${description}" />
+        </a>`
+    )
+    .join('');
+
+  // рендерим галлерею
+  galleryRef.insertAdjacentHTML('beforeend', markup);
+
+  return galleryRef;
+}
