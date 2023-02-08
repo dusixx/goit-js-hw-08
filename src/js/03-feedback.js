70ede1cc9475075f36a2df7b1a6e19e8655bc32d
dusixx <113414935+dusixx@users.noreply.github.com>
2023-02-08 03:26:33 +0200

    adds task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
+import { FormDataStorage } from './form-data';
 
+const THROTTLE_PERIOD = 500;
+const formData = new FormDataStorage('.feedback-form', 'feedback-form-state');
+const form = formData.target;
+
+// делаем поле email обязательным для заполнения
+form.email.setAttribute('required', '');
+
+// заполняем форму из хранилища
+formData.restore();
+
+form.addEventListener(
+  'input',
+  throttle(() => formData.save(), THROTTLE_PERIOD)
+);
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  formData.log();
+  formData.clear();
+});
---
+++ src/js/form-data.js
@@ -0,0 +1,45 @@
+export class FormDataStorage {
+  #key;
+
+  constructor(formSelector, key) {
+    this.target = document.querySelector(formSelector);
+    this.#key = key;
+  }
+
+  get() {
+    const { email, message } = this.target;
+    // если не заполнен email - данные не валидны
+    return (
+      email.value && {
+        email: email.value,
+        message: message.value,
+      }
+    );
+  }
+
+  save() {
+    const formData = this.get();
+    if (formData) localStorage.setItem(this.#key, JSON.stringify(formData));
+  }
+
+  restore() {
+    const formData = JSON.parse(localStorage.getItem(this.#key) || 'null');
+
+    if (formData) {
+      const { email, message } = this.target;
+
+      email.value = formData.email;
+      message.value = formData.message;
+    }
+  }
+
+  clear() {
+    this.target.reset();
+    localStorage.removeItem(this.#key);
+  }
+
+  log() {
+    const formData = this.get();
+    if (formData) console.log(formData);
+  }
+}
